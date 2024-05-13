265c52c1f897ebe5bf6456330c36572b5048c081
boak123 <boluwatifeakorede17@gmail.com>
2024-05-13 14:47:39 -0700

    First API

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+// file AppController.js that contains the definition of the 2 endpoints
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
+  }
+
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
+  }
+}
+
+export default AppController;
